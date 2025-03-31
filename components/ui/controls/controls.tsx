a0d46b1fc9d64ff4028d8654ce88a50635983112
krawtschenko <eugenykravchenko@gmail.com>
2025-03-31 23:58:11 +0200

    feat: moved options

--- components/ui/controls/controls.tsx
+++ components/ui/controls/controls.tsx
@@ -3,16 +3,16 @@ import { Search } from '@/components/ui/search/search';
 import { useState } from 'react';
 import { Select, SelectItem } from '@/components/ui/select/select';
 
+const options = [
+  { value: 'Africa', label: 'Africa' },
+  { value: 'America', label: 'America' },
+  { value: 'Asia', label: 'Asia' },
+  { value: 'Europe', label: 'Europe' },
+  { value: 'Oceania', label: 'Oceania' },
+];
+
 export const Controls = () => {
   const [search, setSearch] = useState('');
-
-  const options = [
-    { value: 'Africa', label: 'Africa' },
-    { value: 'America', label: 'America' },
-    { value: 'Asia', label: 'Asia' },
-    { value: 'Europe', label: 'Europe' },
-    { value: 'Oceania', label: 'Oceania' },
-  ];
 
   return (
     <div>
